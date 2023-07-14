bc3ec2cb9e5612528cdb6319657a4df4139edde2
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-07-14 14:30:03 +0530

    Navbar Component creater

---
+++ dashboard-front/src/scenes/layout/index.jsx
@@ -0,0 +1,19 @@
+import { Box, useMediaQuery } from '@mui/material';
+import Navbar from 'components/Navbar';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+
+const Layout = props => {
+
+    return (
+        <Box width="100%" height="100%" >
+            <Box>
+                <Navbar/>
+                <Outlet/>
+            </Box>
+        </Box>
+    );
+};
+
+export default Layout;
