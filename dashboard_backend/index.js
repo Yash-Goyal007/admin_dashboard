282defd0e7c8a1b902e3df3d13a4b963771ef9c1
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-07-18 13:01:03 +0530

    Transactions page completed

--- dashboard_backend/index.js
+++ dashboard_backend/index.js
@@ -14,8 +14,9 @@ import salesRoutes from './routes/sales.js';
 //data imports
 import User from './models/User.js';
 import Product from './models/Product.js';
-import ProductStat from './models/ProductStat.js'
-import { dataUser, dataProduct, dataProductStat } from './data/index.js';
+import ProductStat from './models/ProductStat.js';
+import Transaction from './models/Transaction.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js';
 
 //Configuration
 dotenv.config();
@@ -44,6 +45,7 @@ mongoose.connect(process.env.MONGO_URL, {
     // User.insertMany(dataUser);
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
 }).catch(error => {
     console.log(`${error} did not connect`);
 })
