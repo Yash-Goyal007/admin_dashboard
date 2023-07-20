57310690ab4d44d49013bf864d4663b899f63466
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-07-20 13:25:04 +0530

    Geography page completed

--- dashboard-front/src/App.js
+++ dashboard-front/src/App.js
@@ -9,6 +9,7 @@ import Dashboard from 'scenes/dashboard'
 import Products from 'scenes/products';
 import Customers from 'scenes/customers';
 import Transactions from 'scenes/transactions';
+import Geography from 'scenes/geography';
 
 function App() {
   const mode = useSelector((state => state.global.mode));
@@ -19,12 +20,13 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route element={<Layout/>}>
-              <Route path ="/" element = {<Navigate to="/dashboard" replace/>} />
-              <Route path ="/dashboard" element = {<Dashboard/>} />
-              <Route path ="/products" element = {<Products />} />
-              <Route path ="/customers" element = {<Customers />} />
-              <Route path ="/transactions" element = {<Transactions />} />
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
             </Route>
           </Routes>
         </ThemeProvider>
---
+++ dashboard-front/src/scenes/geography/index.jsx
@@ -0,0 +1,93 @@
+import { Box, useTheme } from '@mui/material';
+import { ResponsiveChoropleth } from '@nivo/geo';
+import Header from 'components/Header';
+import React from 'react';
+import { useGetGeographyQuery } from 'state/api';
+import { geoData } from 'state/geoData';
+
+const Geography = props => {
+    const theme = useTheme();
+    const { data } = useGetGeographyQuery();
+    return (
+        <Box m="1.5rem 2.5rem">
+            <Header title="GEOGRAPHY" subtitle="Find where your users are located." />
+            <Box mt="40px" height="75vh" border={`1px solid ${theme.palette.secondary[200]}`} borderRadius="4px" >
+                {data ? <ResponsiveChoropleth
+                    data={data}
+                    theme={{
+                        axis: {
+                            domain: {
+                                line: {
+                                    stroke: theme.palette.secondary[200]
+                                }
+                            },
+                            legend: {
+                                text: {
+                                    fill: theme.palette.secondary[200]
+                                }
+                            },
+                            ticks: {
+                                line: {
+                                    stroke: theme.palette.secondary[200],
+                                    strokeWidth: 1
+                                },
+                                text: {
+                                    fill: theme.palette.secondary[200]
+                                }
+                            }
+                        },
+                        legends: {
+                            text: {
+                                fill: theme.palette.secondary[200]
+                            }
+                        },
+                        tooltip: {
+                            container: {
+                                color: theme.palette.primary.main
+                            }
+                        }
+                    }}
+                    features={geoData.features}
+                    margin={{ top: 0, right: 0, bottom: 0, left: -50 }}
+                    colors="nivo"
+                    domain={[0, 60]}
+                    unknownColor="#666666"
+                    label="properties.name"
+                    valueFormat=".2s"
+                    projectionScale={135}
+                    projectionTranslation={[0.5, 0.65]}
+                    projectionRotation={[-10, 0, 0]}
+                    borderWidth={1.3}
+                    borderColor="#ffffff"
+                    legends={[
+                        {
+                            anchor: 'bottom-left',
+                            direction: 'column',
+                            justify: true,
+                            translateX: 920,
+                            translateY: -50,
+                            itemsSpacing: 0,
+                            itemWidth: 94,
+                            itemHeight: 18,
+                            itemDirection: 'left-to-right',
+                            itemTextColor: theme.palette.secondary[200],
+                            itemOpacity: 0.85,
+                            symbolSize: 18,
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemTextColor: theme.palette.background.alt,
+                                        itemOpacity: 1
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                /> : <>Loading...</>}
+            </Box>
+        </Box>
+    );
+};
+
+export default Geography;
