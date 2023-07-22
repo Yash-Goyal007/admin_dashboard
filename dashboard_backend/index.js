76c952267212298db6419bf4bb9dd97accad7bc1
Yash Goyal <yashgoyalmrt1912@gmail.com>
2023-07-22 21:23:00 +0530

    Performance Page completed

--- dashboard_backend/index.js
+++ dashboard_backend/index.js
@@ -17,7 +17,8 @@ import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
 import OverallStat from './models/OverallStat.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
+import AffiliateStat from './models/AffiliateStat.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from './data/index.js';
 
 //Configuration
 dotenv.config();
@@ -48,6 +49,7 @@ mongoose.connect(process.env.MONGO_URL, {
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     // OverallStat.insertMany(dataOverallStat);
+    // AffiliateStat.insertMany(dataAffiliateStat); 
 }).catch(error => {
     console.log(`${error} did not connect`);
 })
